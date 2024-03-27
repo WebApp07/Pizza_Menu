1659999d29e04e8aa7366918e2b3f9fb89b898ec
WebApp07 <aminezouguig@proton.me>
2024-03-27 05:42:45 -0700

    Creating Components

--- src/data.js
+++ src/data.js
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 const pizzaData = [
   {
     name: "Focaccia",
@@ -45,35 +42,3 @@ const pizzaData = [
     soldOut: false,
   },
 ];
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <Menu />
-      <Footer />
-    </div>
-  );
-}
-
-function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
-}
-function Menu() {
-  return (
-    <div>
-      <h2>Our menu</h2>
-    </div>
-  );
-}
-function Footer() {
-  return <footer>{new Date().toDateString()} We're currently open</footer>;
-}
-
-// React V18
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
