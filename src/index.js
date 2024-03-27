40a1a5891cf8b8a9d1495f7598628fb580cf38b8
WebApp07 <aminezouguig@proton.me>
2024-03-27 06:23:48 -0700

    Passing and Receiving data

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ function App() {
 
 function Header() {
   return (
-    <header className="header">
+    <header className="header footer">
       <h1>Fast React Pizzs Co.</h1>
     </header>
   );
@@ -69,9 +69,43 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, Mozarella, Spinach and Ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={15}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price={12}
+      />
+
+      <Pizza
+        name="Pizza Salamino"
+        ingredients="Tomato, mozarella, and pepperoni"
+        photoName="pizzas/salamino.jpg"
+        price={10}
+      />
     </main>
   );
 }
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
+  );
+}
+
 function Footer() {
   const hour = new Date().getHours();
   const openHour = 5;
