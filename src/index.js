d67c9e56ac04842812d59c2d1640c7adef286fe5
WebApp07 <aminezouguig@proton.me>
2024-03-28 08:16:53 -0700

    Rendring Pizzas

--- src/index.js
+++ src/index.js
@@ -69,14 +69,23 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza
+
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+
+      {/* 
+
+       <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, Mozarella, Spinach and Ricotta cheese"
         photoName="pizzas/spinaci.jpg"
         price={15}
       />
-
-      <Pizza
+      
+       <Pizza
         name="Pizza Funghi"
         ingredients="Tomato, mozarella, mushrooms, and onion"
         photoName="pizzas/funghi.jpg"
@@ -88,21 +97,21 @@ function Menu() {
         ingredients="Tomato, mozarella, and pepperoni"
         photoName="pizzas/salamino.jpg"
         price={10}
-      />
+      />*/}
     </main>
   );
 }
 
 function Pizza(props) {
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
